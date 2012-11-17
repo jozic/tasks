283b59f01e26f4c3c5803a29a54cf4aca3f8f44a
jozic <jozic@ua.fm>
2012-11-17 17:17:56 -0500

    save/load tasks to/from flat file

--- src/main/scala/ua/com/jozic/Tasks.scala
+++ src/main/scala/ua/com/jozic/Tasks.scala
@@ -1,19 +1,27 @@
 package ua.com.jozic
 
 import annotation.tailrec
+import java.io._
+import io.Source
 
 /**
  * @author jozic
  * @since 11/16/12
  */
-class Tasks {
+class Tasks(tasksSeq: (String, String)*) {
 
   private val tasks = collection.mutable.ArrayBuffer[Task]()
   private var max = 0
 
-  def +(name: String) = {
-    tasks += Task(name, 0)
-    max = math.max(name.length, max)
+  tasksSeq foreach {
+    case (name, progress) => add(Task(name, progress.toInt))
+  }
+
+  def add(name: String): Tasks = add(Task(name, 0))
+
+  def add(task: Task): Tasks = {
+    tasks += task
+    max = math.max(task.name.length, max)
     this
   }
 
@@ -22,12 +30,15 @@ class Tasks {
     this
   }
 
-  def list() {
-    println(this)
+  def clear() = {
+    tasks.clear()
+    this
   }
 
   case class Task(name: String, progress: Int) {
     override def toString: String = name + " : " + "".padTo(progress, '#')
+
+    def toStoreString: String = name + ":" + progress
 
     def inc = copy(progress = progress + 1)
   }
@@ -38,32 +49,53 @@ class Tasks {
     }
     formatted.mkString("\n")
   }
+
+  def toStoreString: String = tasks.map(_.toStoreString).mkString("\n")
 }
 
 object Tasks extends App {
 
-  val tasks = new Tasks
+  val filename = "tasks.lst"
+  val tasks = load()
 
-  tasks + "small task 1"
-  tasks + "task 2"
-  tasks + "task 3"
-  tasks + "huge task 4"
+  def load(): Tasks = {
+    val loaded = if (new File(filename).exists())
+      for {
+        line <- Source.fromFile(filename).getLines()
+        (name, progress) = line.splitAt(line.indexOf(':'))
+      } yield (name, progress.drop(1))
+    else Seq.empty
+    new Tasks(loaded.toSeq: _*)
+  }
 
-  tasks inc 1
-  tasks inc 2
+  def save(tasks: Tasks): Tasks = {
+    val writer: FileWriter = new FileWriter(filename)
+    try {
+      writer.write(tasks.toStoreString)
+    } finally {
+      writer.close()
+    }
+    tasks
+  }
 
-  tasks.list()
+  def list() {
+    println(tasks)
+  }
 
   @tailrec
   def listen() {
     readLine("> ") match {
       case ":q" => println("buy")
-      case ":l" => tasks.list(); listen()
+      case ":l" => list(); listen()
+      case ":drop" => tasks.clear(); save(tasks); listen()
       case n if n.matches("\\d+") =>
-        tasks.inc(n.toInt - 1); tasks.list(); listen()
+        tasks.inc(n.toInt - 1); save(tasks); list(); listen()
+      case name if name.matches("\".+\"") =>
+        tasks add name.stripPrefix("\"").stripSuffix("\""); save(tasks); list(); listen()
       case _ => listen()
     }
   }
 
+  list()
   listen()
 }
